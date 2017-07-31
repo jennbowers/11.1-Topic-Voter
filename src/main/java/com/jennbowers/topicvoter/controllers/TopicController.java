8bb9dc30cdc56f97dc601a1d0f0ceade11f53553
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-31 10:12:55 -0400

    Created models and had information display on screen

--- src/main/java/com/jennbowers/topicvoter/controllers/TopicController.java
+++ src/main/java/com/jennbowers/topicvoter/controllers/TopicController.java
@@ -1,12 +1,27 @@
 package com.jennbowers.topicvoter.controllers;
 
+import com.jennbowers.topicvoter.models.Topic;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class TopicController {
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Topic first = new Topic(1, "Java Spring", "Not sure how Spring works");
+        Topic second = new Topic(2, "Backend fundamentals", "Node on the backend?! What!!");
+        Topic third = new Topic(3, "React & Redux", "What is a single page app anyways??");
+
+        ArrayList<Topic> topics = new ArrayList<>();
+        topics.add(first);
+        topics.add(second);
+        topics.add(third);
+
+        model.addAttribute("topics", topics);
+
         return "index";
     }
 }
