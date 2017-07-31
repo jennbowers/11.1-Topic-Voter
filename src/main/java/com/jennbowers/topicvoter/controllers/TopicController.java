fbf2d2141dd040740c5caf0377541fb4c34c3991
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-31 11:15:02 -0400

    Database integration for find all complete

--- src/main/java/com/jennbowers/topicvoter/controllers/TopicController.java
+++ src/main/java/com/jennbowers/topicvoter/controllers/TopicController.java
@@ -1,27 +1,25 @@
 package com.jennbowers.topicvoter.controllers;
 
 import com.jennbowers.topicvoter.models.Topic;
+import com.jennbowers.topicvoter.repositories.TopicRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TopicController {
+
+    @Autowired
+    private TopicRepositoryImpl repo;
+
     @RequestMapping("/")
     public String index(Model model) {
-        Topic first = new Topic(1, "Java Spring", "Not sure how Spring works");
-        Topic second = new Topic(2, "Backend fundamentals", "Node on the backend?! What!!");
-        Topic third = new Topic(3, "React & Redux", "What is a single page app anyways??");
-
-        ArrayList<Topic> topics = new ArrayList<>();
-        topics.add(first);
-        topics.add(second);
-        topics.add(third);
-
-        model.addAttribute("topics", topics);
-
+        List<Topic> allTopics = repo.findAll();
+        model.addAttribute("topics", allTopics);
         return "index";
     }
 }
